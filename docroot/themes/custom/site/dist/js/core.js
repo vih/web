cc490e024db7a4da09c2f0a16152073bad971e2e
Morten Nissen <mn@bellcom.dk>
2017-01-11 15:54:38 +0100

    Use the systems modernizr

--- docroot/themes/custom/site/dist/js/core.js
+++ docroot/themes/custom/site/dist/js/core.js
@@ -283,53 +283,6 @@ var pageLayout = (function ($) {
     return pub;
 })(jQuery);
 
-// |--------------------------------------------------------------------------
-// | Header
-// |--------------------------------------------------------------------------
-// |
-// | This jQuery script is written by
-// | Morten Nissen
-// |
-
-var header = (function ($) {
-    'use strict';
-
-    var pub = {};
-
-    /**
-     * Instantiate
-     */
-    pub.init = function () {
-        registerBootEventHandlers();
-        registerEventHandlers();
-    };
-
-    /**
-     * Register boot event handlers
-     */
-    function registerBootEventHandlers() {}
-
-    /**
-     * Register event handlers
-     */
-    function registerEventHandlers() {
-
-        if ( ! Modernizr.touchevents) {
-            var $element = $('#header'),
-                scrollTop     = $(window).scrollTop(),
-                elementOffset = $element.find('.header-bottom-inner').offset().top,
-                distance      = (elementOffset - scrollTop);
-
-            $element.sticky({
-                topSpacing: 0,
-                className: 'header-is-sticky'
-            });
-        }
-    }
-
-    return pub;
-})(jQuery);
-
 // Document ready
 (function ($) {
     'use strict';
