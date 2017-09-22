7a13517fd688fd623c989521994ab98705227fb2
Dmitri Serõn <ds@bellcom.ee>
2017-09-22 12:44:53 +0300

    VI-127 search field must replace the main menu

--- docroot/themes/custom/site/dist/js/core.js
+++ docroot/themes/custom/site/dist/js/core.js
@@ -311,7 +311,7 @@ var modalPopUp = (function ($) {
     }
 
     $(".close").click(function () {
-        $.cookie('no_thanks', 'true', {expires: 10, path: '/'});
+        $.cookie('no_thanks', 'true', {expires: 36500, path: '/'});
     });
 
     if ($.cookie("no_thanks") !== true) {
@@ -336,27 +336,35 @@ var modalPopUp = (function ($) {
 
 // Document ready
 (function ($) {
-    'use strict';
+  'use strict';
 
-    // Enable layout
-    layout.init();
+  // Enable layout
+  layout.init();
 
-    //Modal PopUp
-    modalPopUp.init();
+  //Modal PopUp
+  modalPopUp.init();
 
-    $("#toggle_mobile_menu").click(function(event){
-      $('#main-menu').toggleClass('mobile-menu-open');
-      $('.layout__document').toggleClass('mobile-menu-open');
-    })
-    $(".search-button").popover({
-    html : true,
-    trigger: 'click',
-    placement : 'bottom',
-    content: function() {
-      return $("#search-form-popover").html();
+  $("#toggle_mobile_menu").click(function (event) {
+    $('#main-menu').toggleClass('mobile-menu-open');
+    $('.layout__document').toggleClass('mobile-menu-open');
+
+  })
+
+//Show search form block
+  $(".search-button").click(function (event) {
+    if ($("#search-form-popover").hasClass("hidden")) {
+      $("#search-form-popover").removeClass('hidden');
     }
   });
 
+//Hide search form block
+  $(document).click(function (event) {
+    if (!$(event.target).closest('#search-form-popover').length && !$(event.target).closest('.search-button').length) {
+      if (!$("#search-form-popover").hasClass("hidden")) {
+        $("#search-form-popover").addClass('hidden');
+      }
+    }
+  });
 
 })(jQuery);
 
