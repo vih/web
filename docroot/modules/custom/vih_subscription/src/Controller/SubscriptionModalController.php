0159132093f2d7c88fa11abed6c2764c36a3d894
Dmitri Serõn <ds@bellcom.ee>
2018-06-18 11:19:16 +0300

    VI-441 Banner on node and term modals fix.

--- docroot/modules/custom/vih_subscription/src/Controller/SubscriptionModalController.php
+++ docroot/modules/custom/vih_subscription/src/Controller/SubscriptionModalController.php
@@ -14,27 +14,20 @@ use Drupal\Core\Controller\ControllerBase;
 class SubscriptionModalController extends ControllerBase {
 
   public function vih_cs_classes($class) {
-
     $term = \Drupal::entityTypeManager()->getStorage('taxonomy_term')->load($class);
-    
-//    $options = [
-//      'dialogClass' => 'popup-dialog-class',
-//      'width' => '50%',
-//    ];
-//    
-//    $content['#markup'] = 'text';
-//    $content['#attached']['library'][] = 'core/drupal.dialog.ajax';
-//    $title = '<div>ttt</div>Here is your content in modal';
-//    $response = new AjaxResponse();
-//    $response->addCommand(new OpenModalDialogCommand($title, $content, $options));
-//    return $response;
-    
     $output = \Drupal::entityTypeManager()
-                    ->getViewBuilder('taxonomy_term')
-                    ->view($term, 'modal_window');
-    
+        ->getViewBuilder('taxonomy_term')
+        ->view($term, 'modal_window');
+
     return $output;
-    
-    
+  }
+
+  public function vih_node_modal($node) {
+
+    $output = \Drupal::entityTypeManager()
+        ->getViewBuilder('node')
+        ->view($node, 'modal_window');
+
+    return $output;
   }
 }
